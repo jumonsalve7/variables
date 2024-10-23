c85d567dbe849762f274be4f553074c1e9edb26b
Juan Pablo Monsalve <85030158+jumonsalve7@users.noreply.github.com>
2024-10-23 10:04:03 +0100

    Add files via upload

---
+++ clase-variables.js
@@ -0,0 +1,17 @@
+
+
+let string1 = "Hola mundo"
+let string2 = "te quiero  mucho"
+let string3 = string1 + " " + string2
+let string4 = `${string1} feliz :)`
+
+//console.log(string1)
+//console.log(string2)
+//console.log(string3)
+//console.log(string4)
+
+let frase = "JavaScript es extremadamente genial"
+console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0,10))
